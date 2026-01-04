ef04070b57fffbd84ff1d20c64ce0b4fb39e6ed7
Hossein <ho.saeidiwork@gmail.com>
2026-01-04 16:58:32 +0330

    changed the entrypoint.sh a little

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,37 @@
 #!/bin/sh
 set -e
 
+python - <<'PY'
+import os
+import time
+import psycopg
+
+host = os.getenv("DJANGO_DB_HOST", "db")
+port = int(os.getenv("DJANGO_DB_PORT", "5432"))
+name = os.getenv("DJANGO_DB_NAME", "nousnews")
+user = os.getenv("DJANGO_DB_USER", "nousnews")
+password = os.getenv("DJANGO_DB_PASSWORD", "nousnews")
+
+for attempt in range(1, 31):
+    try:
+        with psycopg.connect(
+            host=host,
+            port=port,
+            dbname=name,
+            user=user,
+            password=password,
+        ):
+            break
+    except Exception:
+        if attempt == 30:
+            raise
+        time.sleep(1)
+PY
+
 python manage.py makemigrations articles crawler --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py add_seeds --noinput
+python manage.py add_seeds
 python manage.py shell <<'PY'
 from django.contrib.auth import get_user_model
 
